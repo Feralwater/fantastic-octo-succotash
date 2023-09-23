e72789d3a2a4eee41a52eac1403d568dc2efaa6b
BuyakovaVA <lineviktoriya@gmail.com>
2023-09-23 23:26:49 +0200

    add getRepo request

--- src/api/repos.ts
+++ src/api/repos.ts
@@ -28,3 +28,24 @@ export const getReposList = (): Promise<IReposList> => {
     }, 1000);
   });
 };
+
+export const getRepo = (id: string): Promise<IRepo> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      fetch("/repo.json")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          const repo = data.repositories.find((repo: IRepo) => repo.id === id);
+          resolve(repo);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }, 1000);
+  });
+}
